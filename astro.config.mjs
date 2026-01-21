3cf6308ce8a9fe3c3275185f1ed8519ce64d7513
Ruben Boogaard <rubenboogaard@MacBook-Air-van-Ruben.local>
2026-01-21 12:15:53 +0100

    Enable prefixDefaultLocale to fix /nl/ routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     defaultLocale: "nl",
     locales: ["nl", "en", "de"],
     routing: {
-      prefixDefaultLocale: false
+      prefixDefaultLocale: true
     }
   },
   site: 'https://slapenopeeneiland.nl',
